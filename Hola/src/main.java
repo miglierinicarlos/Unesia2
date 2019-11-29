f79a6059fc1de0e8101c5c7db208210a2a0cc33f
miglierinicarlos <miglierinicarlos@gmail.com>
2019-11-29 04:14:17 -0300

    BorrandoClase y modificando main

--- Hola/src/main.java
+++ Hola/src/main.java
@@ -2,13 +2,8 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		Hola p = new Hola(1,2,"Carlos");
-		
-	
-		
-		System.out.println(p.cantidad("Hola"));
+		System.out.println("Hola");
 
 		
 	}
