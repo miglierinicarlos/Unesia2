159175c331f5c0678d1d9e68892d0e9900d8a2d1
miglierinicarlos <miglierinicarlos@gmail.com>
2019-11-29 03:03:26 -0300

    Version inicial de prueba

---
+++ Hola/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Hola p = new Hola(1,2,"Carlos");
+		
+	
+		
+		System.out.println(p.cantidad("Hola"));
+
+		
+	}
+
+}
