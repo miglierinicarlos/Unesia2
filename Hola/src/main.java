49d0ba005cb15b569c7480053d97762542edaab4
miglierinicarlos <miglierinicarlos@gmail.com>
2019-12-06 17:42:53 -0300

    Nuevo mens

--- Hola/src/main.java
+++ Hola/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("mi commit nuevo desde eclipse 2020");
+		System.out.println("mi commit nuevo desde eclipse 202020");
 
 		
 	}
