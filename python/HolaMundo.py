5677e5216bd53f11dfaf9c3b6a756e0f7083f3f4
miglierinicarlos <miglierinicarlos@gmail.com>
2020-04-18 01:18:40 -0300

    Agregue un if

--- python/HolaMundo.py
+++ python/HolaMundo.py
@@ -6,5 +6,7 @@ while (i<3):
 	print("El valor de i es, ",i)
 
 print("NO acpeto cambios de otro usarios")
-
-
+if  (5>7):
+	print("Entro a la opcion verdadera")
+else:	
+	print("Entro a la opcion falsa")
